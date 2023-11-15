ffdc2f06be95cff4945babfc503d78b4d23ec014
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-11-15 12:52:36 +0500

    Add project files.

---
+++ Tyuiu.KosovAS.Sprint5.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.KosovAS.Sprint5.Task0.V22.Lib;
+
+namespace Tyuiu.KosovAS.Sprint5.Task0.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            string path = @" C:\Users\andre\source\repos\Tyuiu.KosovAS.Sprint5\Tyuiu.KosovAS.Sprint5.Task0.V22\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KosovAS.Sprint5.Task0.V22/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KosovAS.Sprint5.Task0.V22.Lib;
+
+namespace Tyuiu.KosovAS.Sprint5.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+
+            Console.Title = "Спринт #5 | Выполнил: Косов А. С. | АСОиУБ-23-3";
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* Спринт #5" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Вариант #22" + String.Concat(Enumerable.Repeat(" ", 61)) + "*");
+            Console.WriteLine("* Выполнил: Косов Андрей Сергеевич | АСОиУБ-23-3" + String.Concat(Enumerable.Repeat(" ", 26)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
+            Console.WriteLine("* Написать програму, которая вычисляет данные по формуле, сохраняет файл  *");
+            Console.WriteLine("* и печатает их на экран.                                                 *");
+            Console.WriteLine("*" + String.Concat(Enumerable.Repeat(" ", 73)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            Console.WriteLine($"x = {x}");
+
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine($"* Результат:{String.Concat(Enumerable.Repeat(" ", 62))}*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            string result = ds.SaveToFileTextData(x);
+
+            Console.WriteLine($"Файл: {result}");
+            Console.WriteLine("Создан!");
+            Console.WriteLine($"Значение в файле:\n{File.ReadAllText(result)}");
+            Console.ReadKey();
+        }
+    }
+}
