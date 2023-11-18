5708a25828f206c3905a160cc8ffa3b7459a4166
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-11-18 23:35:30 +0500

    Task2

---
+++ Tyuiu.KosovAS.Sprint5.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Tyuiu.KosovAS.Sprint5.Task2.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\andre\source\repos\Tyuiu.KosovAS.Sprint5\Tyuiu.KosovAS.Sprint5.Task2.V1\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
